5240c88191ff9d4b7ec1a8f9ad32e9b9812d2ccc
princy1906 <princypatel19062002@gmail.com>
2026-08-21 18:05:17 +0530

    1) Add Post Deployement Script 2)Add Status table

---
+++ DatabaseProjectCustomer/dbo/Tables/Status.sql
@@ -0,0 +1,11 @@
+CREATE TABLE dbo.Status
+(
+    StatusId INT IDENTITY(1,1) NOT NULL,
+    StatusName NVARCHAR(50) NOT NULL,
+
+    CONSTRAINT PK_Status
+        PRIMARY KEY (StatusId),
+
+    CONSTRAINT UQ_Status_StatusName
+        UNIQUE (StatusName)
+);
