d6ca0e64ce5dca1729092c70b4682f2f2d2151be
princy1906 <princypatel19062002@gmail.com>
2026-08-25 14:22:06 +0530

    update the defualt value for the roleif

---
+++ DatabaseProjectCustomer/obj/Debug/postdeploy.sql
@@ -0,0 +1,64 @@
+﻿/* ============================================
+   Status Master Data
+   ============================================ */
+
+IF NOT EXISTS
+(
+    SELECT 1
+    FROM [dbo].[Status]
+    WHERE StatusName = 'Active'
+)
+BEGIN
+    INSERT INTO [dbo].[Status] (StatusName)
+    VALUES ('Active');
+END;
+
+IF NOT EXISTS
+(
+    SELECT 1
+    FROM [dbo].[Status]
+    WHERE StatusName = 'Inactive'
+)
+BEGIN
+    INSERT INTO [dbo].[Status] (StatusName)
+    VALUES ('Inactive');
+END;
+
+IF NOT EXISTS
+(
+    SELECT 1
+    FROM [dbo].[Status]
+    WHERE StatusName = 'Suspended'
+)
+BEGIN
+    INSERT INTO [dbo].[Status] (StatusName)
+    VALUES ('Suspended');
+END;
+
+IF NOT EXISTS
+(
+    SELECT 1
+    FROM [dbo].[Status]
+    WHERE StatusName = 'Pending'
+)
+BEGIN
+    INSERT INTO [dbo].[Status] (StatusName)
+    VALUES ('Pending');
+END;
+
+
+
+/* Add Role Master Data */
+
+IF NOT EXISTS (SELECT 1 FROM [dbo].[Role] WHERE [RoleName] = 'Admin')
+BEGIN
+    INSERT INTO [dbo].[Role] ([RoleId], [RoleName])
+    VALUES ($(AdminRoleId), 'Admin');
+END;
+
+IF NOT EXISTS (SELECT 1 FROM [dbo].[Role] WHERE [RoleName] = 'Owner')
+BEGIN
+    INSERT INTO [dbo].[Role] ([RoleId], [RoleName])
+    VALUES ($(OwnerRoleId), 'Owner');
+END;
+GO
